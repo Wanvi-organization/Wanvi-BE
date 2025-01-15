23cafc886de86db7fb1312ff9d6ca3c64f5387d9
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-01-15 21:58:16 +0700

    add docker

--- Wanvi.API/Program.cs
+++ Wanvi.API/Program.cs
@@ -28,11 +28,9 @@ builder.Services.AddConfig(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
