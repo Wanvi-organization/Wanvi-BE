c370fa83bcdaab9cdf038b4955f883b143327bb9
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-02-04 14:50:27 +0700

    fix

--- Wanvi.API/Program.cs
+++ Wanvi.API/Program.cs
@@ -15,10 +15,6 @@ builder.Configuration
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-//Add services to the container.
-//builder.Services.AddDbContext<DatabaseContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
