88afb38951a66c6f3a57af4e4feb098d17e5829e
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-02-04 15:13:14 +0700

    fix web

--- Wanvi.API/Program.cs
+++ Wanvi.API/Program.cs
@@ -15,6 +15,7 @@ builder.Configuration
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
