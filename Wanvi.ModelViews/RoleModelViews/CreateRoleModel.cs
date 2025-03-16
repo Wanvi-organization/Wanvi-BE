470d3fe6acd0970d248e80a3b01123ba135b7317
truongdaivy57 <truongdaivy57@gmail.com>
2025-03-16 11:32:17 +0700

    feat: CRUD role

---
+++ Wanvi.ModelViews/RoleModelViews/CreateRoleModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wanvi.ModelViews.RoleModelViews
+{
+    public class CreateRoleModel
+    {
+        [Required(ErrorMessage = "Tên vai trò không được để trống.")]
+        public string Name { get; set; }
+    }
+}
---
+++ Wanvi.ModelViews/RoleModelViews/UpdateRoleModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wanvi.ModelViews.RoleModelViews
+{
+    public class UpdateRoleModel
+    {
+        [Required(ErrorMessage = "Tên vai trò không được để trống.")]
+        public string Name { get; set; }
+    }
+}
